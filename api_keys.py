ef52a748b2818b65c32ca910c9222e7f5c54b86c
avistechqwn <107156165+avistechqwn@users.noreply.github.com>
2023-07-30 18:52:10 -0400

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "f338a58846cd6b3dd26722a63e53bc77"
+
+# Geoapify API Key
+#geoapify_key = "66fa6f4dbbb74bd28b202539bd976790"
+geoapify_key = "cbd2df9809474d5baea5645ff1e82e62"
